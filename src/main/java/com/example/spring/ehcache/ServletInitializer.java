6142a7f434295b641526aeade142757724227fa9
sunboyu <sunboyu@jd.com>
2018-01-25 15:41:30 +0800

    create new branch

---
+++ src/main/java/com/example/spring/ehcache/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.spring.ehcache;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringehcacheApplication.class);
+	}
+
+}
